3b721cfe893c23ccc1280de503a39446ca6cd60c
amarsh30 <rzhamr06@gmail.com>
2022-07-10 23:42:16 +0700

    up

--- dashboard.php
+++ dashboard.php
@@ -28,14 +28,14 @@ if (!isset($_SESSION['id_pelajar'])) {
 <header>
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#">KANTIN KEJUJURAN</a>
+				<a class="navbar-brand" href="dashboard.php">KANTIN KEJUJURAN</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
 				<?php $query = $conn->query("SELECT * FROM balance_box"); while ($row = $query->fetch_assoc()): ?>
-					<a class="nav-link active" aria-current="page" href="#">Home</a>
+					<a class="nav-link active" aria-current="page" href="dashboard.php">Home</a>
 					<a class="nav-link" href="tambah.php">Tambah Produk</a>
 					<a class="nav-link" href="withdraw.php"> Rp.<?= number_format($row["jumlah"]); ?></a>
 					<a class="nav-link" href="logout.php">Logout</a>
